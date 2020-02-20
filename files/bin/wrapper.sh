563f6ad519eb51c1c304d53c42a54d9d1c17a7c4
Joe McManus <joe@highstormsecurity.com>
2020-02-20 07:36:40 -0700

    Added some comments and help messages

--- files/bin/wrapper.sh
+++ files/bin/wrapper.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-
+#bash script to launch the php server. 
 
 case $1 in 
 
 startweb)
 	php7.2 -S 0.0.0.0:1984 -t $SNAP/phpweb  >/dev/null 2>&1 & 
 	python3 $SNAP/pyweb/server.py >/dev/null 2>&1 & 
-	echo "Open your browser to this host on port 1984. eg: http://thisHostIP:1984" 
+	echo "Open your browser to this host on port 1984. eg: http://127.0.0.1:1984" 
 
 ;;
 
